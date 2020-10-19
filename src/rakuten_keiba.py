60247483005b44f7646c5783f304c8764ee849dd
ksmulti <ksmulti@users.noreply.github.com>
2020-10-19 22:42:33 +0900

    update
    update

--- src/rakuten_keiba.py
+++ src/rakuten_keiba.py
@@ -3,7 +3,8 @@ import rakuten_search_lib as RSL
 rsc = RSL.RakutenSearchCore()
 rsc.Index()
 rsc.GoLoginPage()
-rsc.Login()
-rsc.Search()
+#rsc.Login()
+#rsc.Search()
+rsc.Quit()
 
 print("rakuten search end")
--- src/rakuten_keiba_lib.py
+++ src/rakuten_keiba_lib.py
@@ -4,18 +4,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 import random
 import time
 
 class RakutenKeibaCore:
     def __init__(self):
         chop = webdriver.ChromeOptions()
-        self.__browser = webdriver.Chrome(chrome_options=chop)    #open chrome browser
+        #chop.add_extension("rakuten_extension.crx")
+        chop.add_argument('--headless')
+        chop.add_argument("--remote-debugging-port=9222")
+        chop.add_argument("--no-sandbox")
+        chop.add_argument("--single-process")
+        chop.add_argument("--disable-setuid-sandbox")
+        self.__browser = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), options=chop)    #open chrome browser
         self.__delay = 10
-        self.__settings = self.LoadSettings()
+        #self.__settings = self.LoadSettings()
+        #self.__keywords = self.LoadKeywords()
 
     def LoadSettings(self):
-        with open('../settings.json', encoding='utf-8') as json_file:
+        with open('settings.json', encoding='utf-8') as json_file:
             settings = json.load(json_file)
             return settings
 
@@ -29,11 +38,18 @@ class RakutenKeibaCore:
     
     def Index(self):
         self.__browser.get("https://keiba.rakuten.co.jp/")
+        print("Loading index OK!")
+
+    def Quit(self):
+        self.__browser.quit()
+        print("Browser quit")
     
     def GoLoginPage(self):
-        btn_login = self.__browser.find_element_by_class_name("siteheader_actionlist_btn")
+        btn_login = self.__browser.find_element_by_xpath("//span[contains(text(), '¤Jª÷')][@class='siteheader_actionlist_label']")
+        print(btn_login.text)
         btn_login.click()
-        self.WaitPageSteady('loginInner_u', By.ID)
+        self.WaitPageSteady('loginInner_u')
+        print("GoLoginPage OK!")
 
     def Login(self):
         edit_user = self.__browser.find_element_by_id("loginInner_u")
@@ -44,6 +60,7 @@ class RakutenKeibaCore:
         btn_login = self.__browser.find_element_by_name("submit")
         btn_login.click()
         self.WaitPageSteady('header_common_inner frheader_clearfix', By.CLASS_NAME)
+        print("Login OK!")
 
     def ChargeMoney(self):
         self.__browser.get("https://bet.keiba.rakuten.co.jp/bank/deposit/")
